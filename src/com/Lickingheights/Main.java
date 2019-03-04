85aae4753bd8bae025fbb134a0f4152a0c875bd0
Alyssa <alyssa.nichols.2022@student.lhschools.org>
2019-03-04 07:58:49 -0500

    Created My First Method

--- src/com/Lickingheights/Main.java
+++ src/com/Lickingheights/Main.java
@@ -3,9 +3,27 @@ package com.Lickingheights;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
         //Make 8 commits
         //Make 6 Methods
+        //1.that doesn't return anything and prints "Hello World"
+        //2.returns "hello" with whatever they type
+        //3.Multiples two given numbers and returns the results
+        //4.Divide two given numbers and returns a double
+        //5.Finds the max number (double) between two values and returns it
+        //6.Switch the order of two words and return it
+
+        printGreetings("World");
+
 
     }
+
+    public static void printGreetings(String noun) {
+
+        System.out.println("Hello " + noun);
+
+
+    }
+
+}
 }
